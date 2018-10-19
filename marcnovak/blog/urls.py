c9a454c9348d5eeef42ee95c588348f9b1680af0
heymoose <marcusanovak@gmail.com>
2018-10-19 15:10:07 -0500

    Add CRUD operations for posts

--- marcnovak/blog/urls.py
+++ marcnovak/blog/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from blog.views import (
     PostListView,
     PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
 )
 
 
 urlpatterns = [
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('new/', PostCreateView.as_view(), name='post_new'),
     path('', PostListView.as_view(), name='post_list'),
 ]
--- marcnovak/blog/views.py
+++ marcnovak/blog/views.py
@@ -16,3 +16,52 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ('title', 'content')
+    template_name = 'blog/post_edit.html'
+    context_object_name = 'post'
+    login_url = 'login'
+
+    # In this case, as with the form_valid function in the create view below,
+    # this is essentially overriding a native UpdateView function called dispatch
+    # to do something custom, then calling the overriden native function
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    context_object_name = 'post'
+    success_url = reverse_lazy('post_list')
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    context_object_name = 'post'
+    fields = ('title', 'content')
+
+    # Since this is using LoginRequiredMixin, this lets django know where
+    # to redirect a user who tries to access this view without being logged in,
+    # in this case the login page
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
